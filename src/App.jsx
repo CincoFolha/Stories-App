29675b8f89328c40c2eeda1cc8cf00fb5806e623
LETRA <pedro.pardim.pc@gmail.com>
2025-11-02 19:57:24 -0300

    refactor: Renomeia o componente principal para StoriesApp e atualiza o layout global

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
-import Header from "./components/Header";
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import './App.css'
 
 const StoriesApp = () => {
-
-  return <Header />;
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-purple-50 to-pink-50">
+      <Header />
+    </div>
+  );
 };
 
 export default StoriesApp;
