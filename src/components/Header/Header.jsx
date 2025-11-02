6beaf910a5f03ab2fb73c7e6b3084817b0b48af8
LETRA <pedro.pardim.pc@gmail.com>
2025-11-02 18:07:54 -0300

    fix: mudando titulo 'Stories App' para 'Stories'

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="bg-white shadowsm border-b border-gray-200">
       <div className="max-w-6xl mx-auto px-4 py-4">
         <h1 className="text-2xl font-bold bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent">
-          Stories App
+          Stories
         </h1>
       </div>
     </div>
