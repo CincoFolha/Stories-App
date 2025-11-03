bff94d335f36fb2ccbf6a1fe7c0d2110f6e0a2b2
LETRA <pedro.pardim.pc@gmail.com>
2025-11-03 00:32:00 -0300

    fix: corigindo error de digitação

--- src/components/StoryList/StoryList.jsx
+++ src/components/StoryList/StoryList.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const StoryList = () => {
-  return ();
+  return (
+    <div className="max-w-6xl mx-auto px-4 py-6">
+    </div>
+  );
 };
 
 export default StoryList;
