0623d88e23e050ff831b6e10d378d52cb84aea06
LETRA <pedro.pardim.pc@gmail.com>
2025-11-03 00:27:22 -0300

    feat: adicionando StoryList

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import StoryList from './components/StoryList';
 import './App.css'
 
 const StoriesApp = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-50 to-pink-50">
       <Header />
+      <StoryList />
     </div>
   );
 };
