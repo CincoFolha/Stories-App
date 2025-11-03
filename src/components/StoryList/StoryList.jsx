4ef7ec8525b46a1299c3672ac1c83789b2d1efad
LETRA <pedro.pardim.pc@gmail.com>
2025-11-03 01:19:21 -0300

    feat: Adicionar AddStoryButton no StoryList

--- src/components/StoryList/StoryList.jsx
+++ src/components/StoryList/StoryList.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import AddStoryButton from './AddStoryButton.jsx';
 
 const StoryList = () => {
   return (
     <div className="max-w-6xl mx-auto px-4 py-6">
+      <div className="flex gap-4 overflow-x-auto pb-4 scrollbar-hide">
+        <AddStoryButton />
+      </div>
     </div>
   );
 };
