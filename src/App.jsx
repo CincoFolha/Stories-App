057012a83941e05e89af4a58f1858ade999b4885
LETRA <pedro.pardim.pc@gmail.com>
2025-11-06 01:46:24 -0300

    fix: corigindo path do import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './components/Header';
-import StoryList from './components/StoryList';
-import './App.css'
+import Header from '@/components/Header';
+import StoryList from '@/components/StoryList';
+import '@/App.css'
 
 const StoriesApp = () => {
   return (
