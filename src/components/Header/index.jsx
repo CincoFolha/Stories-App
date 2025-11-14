48b1163cb574ca5fc436afa8db4c29c53df4c535
LETRA <pedro.pardim.pc@gmail.com>
2025-11-14 00:20:32 -0300

    feat(header): add Header component and styles

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="header-content">
+        <h1 className="header-title">Stories</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
