26f60a8166cd11a026931e14872a77357a2a2591
LETRA <pedro.pardim.pc@gmail.com>
2025-11-14 17:31:31 -0300

    refactor(constants): rename image size constants and add story expiration settings

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,7 @@
 
-export const MAX_WIDTH = 1080;
-export const MAX_HEIGHT = 1920;
-export const EXPIRATION_TIME = 24 * 60 * 60 * 1000;
+export const MAX_IMAGE_WIDTH = 1080;
+export const MAX_IMAGE_HEIGHT = 1920;
+export const IMAGE_QUALITY = 0.9;
+export const STORY_EXPIRATION_TIME = 24 * 60 * 60 * 1000; // 24 hours
+export const EXPIRATION_CHECK_INTERVAL = 60000; // 1 minute
+export const STORAGE_KEY = 'stories';
