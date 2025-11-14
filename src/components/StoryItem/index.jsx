c3333fe500fe2dcf4005565640f41fdc24a70a39
LETRA <pedro.pardim.pc@gmail.com>
2025-11-14 10:41:38 -0300

    feat(story-item): add StoryItem component and styles

---
+++ src/components/StoryItem/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { timeFormatter } from '@/utils/timeFormatter.js';
+import './index.css';
+
+const StoryItem = ({ story, onClick }) => {
+  return (
+    <div className="story-item">
+      <button onClick={onClick} className="story-button">
+        <div className="story-avatar">
+          <div className="story-image-wrapper">
+            <img
+              src={story.image}
+              alt="Story"
+              className="story-image"
+            />
+          </div>
+        </div>
+        <span className="story-time">
+          {timeFormatter(story.timestamp)}
+        </span>
+      </button>
+    </div>
+  );
+};
+
+export default StoryItem;
