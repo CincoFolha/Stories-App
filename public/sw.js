71b34a1be2e9ee69cf6957ad2d08d528515862ea
LETRA <pedro.pardim.pc@gmail.com>
2025-11-26 21:56:36 -0300

    feat(pwa): add icon, manifest and service worker

---
+++ public/sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "stories-app-cache-v1";
+const URLS_TO_CACHE = ["/", "/index.html"];
+
+// Instala o SW e faz cache inicial
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+// Ativa e limpa caches antigos
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+});
+
+// Responde com cache primeiro
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return (
+        cachedResponse ||
+        fetch(event.request).catch(() =>
+          caches.match("/offline.html").then((r) => r)
+        )
+      );
+    })
+  );
+});
+
